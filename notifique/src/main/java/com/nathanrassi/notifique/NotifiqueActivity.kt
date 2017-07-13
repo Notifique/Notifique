7b6a125a1fc9c544be410b40e849602321ab9143
nathanrassi <nathanrassi@live.com>
2017-07-12 21:46:27 -0700

    added layout folder and notifique_activity file, removed label from manifest service, removed string resource, added oncreate for activity

--- notifique/src/main/java/com/nathanrassi/notifique/NotifiqueActivity.kt
+++ notifique/src/main/java/com/nathanrassi/notifique/NotifiqueActivity.kt
@@ -1,5 +1,13 @@
 package com.nathanrassi.notifique
 
 import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
 
-class NotifiqueActivity : AppCompatActivity()
+class NotifiqueActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.notifique_activity)
+    }
+
+}
+
