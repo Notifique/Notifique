ea7583720ae30c247c4ef2c13c118226bf91780e
nathanrassi <nathanrassi@live.com>
2017-07-13 23:00:06 -0700

    removed onbind

--- notifique/src/main/java/com/nathanrassi/notifique/NotificationListener.kt
+++ notifique/src/main/java/com/nathanrassi/notifique/NotificationListener.kt
@@ -1,14 +1,9 @@
 package com.nathanrassi.notifique
 
-import android.content.Intent
-import android.os.IBinder
 import android.service.notification.NotificationListenerService
 import android.service.notification.StatusBarNotification
 
 class NotificationListener() : NotificationListenerService() {
-    override fun onBind(intent: Intent): IBinder {
-      TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
 
     override fun onNotificationPosted(sbn: StatusBarNotification) {
     }
