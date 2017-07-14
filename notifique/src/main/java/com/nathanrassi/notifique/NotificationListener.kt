efa7ac5ebbe5a7e4b0c96e110e638dc0fbc7c394
nathanrassi <nathanrassi@live.com>
2017-07-13 22:45:39 -0700

    Made changes from pull feedback

--- notifique/src/main/java/com/nathanrassi/notifique/NotificationListener.kt
+++ notifique/src/main/java/com/nathanrassi/notifique/NotificationListener.kt
@@ -1,11 +1,20 @@
 package com.nathanrassi.notifique
 
-import android.app.Service
 import android.content.Intent
 import android.os.IBinder
+import android.service.notification.NotificationListenerService
+import android.service.notification.StatusBarNotification
 
-class NotificationListener() : Service() {
-    override fun onBind(p0: Intent?): IBinder {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+class NotificationListener() : NotificationListenerService() {
+    override fun onBind(intent: Intent): IBinder {
+      TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
+
+    override fun onNotificationPosted(sbn: StatusBarNotification) {
+    }
+
+    override fun onNotificationRemoved(sbn: StatusBarNotification) {
+    }
+
+
 }
--- notifique/src/main/java/com/nathanrassi/notifique/NotifiqueActivity.kt
+++ notifique/src/main/java/com/nathanrassi/notifique/NotifiqueActivity.kt
@@ -3,11 +3,13 @@ package com.nathanrassi.notifique
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
+
 class NotifiqueActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.notifique_activity)
     }
+
 
 }
 
