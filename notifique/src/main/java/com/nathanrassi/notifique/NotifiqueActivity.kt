67a38b06f797065856fe3da08907200372d760c6
nathanrassi <nathanrassi@live.com>
2017-07-14 22:35:52 -0700

    working base

--- notifique/src/main/java/com/nathanrassi/notifique/NotifiqueActivity.kt
+++ notifique/src/main/java/com/nathanrassi/notifique/NotifiqueActivity.kt
@@ -1,22 +1,24 @@
 package com.nathanrassi.notifique
 
-import android.support.v7.app.AppCompatActivity
-import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import android.provider.Settings
 import android.support.v4.app.NotificationManagerCompat
+import android.support.v7.app.AppCompatActivity
 
 class NotifiqueActivity : AppCompatActivity() {
+
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.notifique_activity)
     if (!NotificationManagerCompat.getEnabledListenerPackages(this).contains(this.packageName)) {
       //send to notification settings
-      val intent = Intent("android.settings.ACTION_NOTIFICATION_LISTENER_SETTINGS")
+      val intent = Intent(Settings.ACTION_NOTIFICATION_LISTENER_SETTINGS)
       this.startActivity(intent)
     }
   }
 
 }
+
 
