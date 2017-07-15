de546716ea5c0e8054e6132bafeda546edcd59c8
nathanrassi <nathanrassi@live.com>
2017-07-14 20:29:57 -0700

    saving changes and pulling master

--- notifique/src/main/java/com/nathanrassi/notifique/NotificationListener.kt
+++ notifique/src/main/java/com/nathanrassi/notifique/NotificationListener.kt
@@ -2,14 +2,19 @@ package com.nathanrassi.notifique
 
 import android.service.notification.NotificationListenerService
 import android.service.notification.StatusBarNotification
+import android.util.Log
 
 class NotificationListener() : NotificationListenerService() {
 
-    override fun onNotificationPosted(sbn: StatusBarNotification) {
-    }
+  override fun onNotificationPosted(sbn: StatusBarNotification) {
+    val TAG: String = "Notifique"
+    var sbnString = sbn.toString()
+    Log.v(TAG, sbnString)
+  }
 
-    override fun onNotificationRemoved(sbn: StatusBarNotification) {
-    }
+  override fun onNotificationRemoved(sbn: StatusBarNotification) {
+
+  }
 
 
 }
--- notifique/src/main/java/com/nathanrassi/notifique/NotifiqueActivity.kt
+++ notifique/src/main/java/com/nathanrassi/notifique/NotifiqueActivity.kt
@@ -1,15 +1,22 @@
 package com.nathanrassi.notifique
 
 import android.support.v7.app.AppCompatActivity
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
-
+import android.support.v4.app.NotificationManagerCompat
 
 class NotifiqueActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.notifique_activity)
-    }
 
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.notifique_activity)
+    if (!NotificationManagerCompat.getEnabledListenerPackages(this).contains(this.packageName)) {
+      //send to notification settings
+      val intent = Intent("android.settings.ACTION_NOTIFICATION_LISTENER_SETTINGS")
+      this.startActivity(intent)
+    }
+  }
 
 }
 
