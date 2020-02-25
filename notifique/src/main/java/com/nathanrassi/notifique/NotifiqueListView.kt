d417f2da2506cfae00700ec367f8a0e2d876a2fa
Nathan <5959299+Rasmaniandevil@users.noreply.github.com>
2020-02-24 18:01:52 -0700

    swipe 2 delete with Icons
    added trash can icons and changed background color. I dont love it but it is fun for now.

--- notifique/src/main/java/com/nathanrassi/notifique/NotifiqueListView.kt
+++ notifique/src/main/java/com/nathanrassi/notifique/NotifiqueListView.kt
@@ -18,6 +18,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.LinearLayout
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.Observer
 import androidx.paging.LivePagedListBuilder
@@ -57,6 +58,7 @@ internal class NotifiqueListView(
 ) : RecyclerView(context, attributeSet) {
   @Inject lateinit var notifiqueQueries: NotifiqueQueries
   lateinit var onSelectionStateChangedListener: OnSelectionStateChangedListener
+  private var deleteIcon = ContextCompat.getDrawable(context, R.drawable.toolbar_delete)!!
   private val allNotifiques: Query<Notifique>
   private val dataSourceFactory: QueryDataSourceFactory<Notifique>
   private lateinit var liveData: LiveData<PagedList<Notifique>>
@@ -211,16 +213,25 @@ internal class NotifiqueListView(
         actionState: Int,
         isCurrentlyActive: Boolean
       ) {
-        super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive)
         val itemView = viewHolder.itemView
+        val iconMargin = (itemView.height - deleteIcon.intrinsicHeight) / 2
         if (dX > 0) {
           swipeBackground.setBounds(itemView.left, itemView.top, dX.toInt(), itemView.bottom)
+          deleteIcon.setBounds(itemView.left + iconMargin, itemView.top + iconMargin, itemView.left + iconMargin + deleteIcon.intrinsicWidth, itemView.bottom - iconMargin)
         } else {
-          swipeBackground.setBounds(
-              itemView.right + dX.toInt(), itemView.top, itemView.right, itemView.bottom
-          )
+          swipeBackground.setBounds(itemView.right + dX.toInt(), itemView.top, itemView.right, itemView.bottom)
+          deleteIcon.setBounds(itemView.right - iconMargin - deleteIcon.intrinsicWidth, itemView.top + iconMargin, itemView.right - iconMargin, itemView.bottom - iconMargin)
         }
         swipeBackground.draw(c)
+        c.save()
+        if (dX > 0) {
+          c.clipRect(itemView.left, itemView.top, dX.toInt(), itemView.bottom)
+        } else {
+          c.clipRect(itemView.right + dX.toInt(), itemView.top, itemView.right, itemView.bottom)
+        }
+        deleteIcon.draw(c)
+        c.restore()
+        super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive)
       }
     }).apply {
       attachToRecyclerView(this@NotifiqueListView)
