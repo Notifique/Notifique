717e3000a3179b5f9642040eac24882add970fb4
Eric Cochran <Eric@NightlyNexus.com>
2021-10-19 20:52:45 -0400

    Fix ExtendedToolbar. (#91)

--- notifique/src/main/java/com/nathanrassi/notifique/ExtendedToolbar.kt
+++ notifique/src/main/java/com/nathanrassi/notifique/ExtendedToolbar.kt
@@ -1,10 +1,15 @@
 package com.nathanrassi.notifique
 
 import android.content.Context
+import android.os.Build.VERSION.SDK_INT
 import android.util.AttributeSet
+import android.view.View.MeasureSpec.EXACTLY
 import android.view.WindowInsets
 import androidx.appcompat.widget.Toolbar
 
+/**
+ * A Toolbar that extends under the status bar system window.
+ */
 private class ExtendedToolbar(
   context: Context,
   attributes: AttributeSet
@@ -15,7 +20,11 @@ private class ExtendedToolbar(
   private var initialMeasure = true
 
   override fun onApplyWindowInsets(insets: WindowInsets): WindowInsets {
-    extraHeight = insets.systemWindowInsetTop
+    extraHeight = if (SDK_INT >= 30) {
+      insets.getInsets(WindowInsets.Type.systemBars()).top
+    } else {
+      @Suppress("Deprecation") insets.systemWindowInsetTop
+    }
     return super.onApplyWindowInsets(insets)
   }
 
@@ -23,13 +32,21 @@ private class ExtendedToolbar(
     widthMeasureSpec: Int,
     heightMeasureSpec: Int
   ) {
-    super.onMeasure(widthMeasureSpec, heightMeasureSpec)
     if (initialMeasure) {
+      super.onMeasure(widthMeasureSpec, heightMeasureSpec)
       initialHeight = measuredHeight
       initialPadding = paddingTop
       initialMeasure = false
+    } else {
+      super.onMeasure(
+        widthMeasureSpec, MeasureSpec.makeMeasureSpec(initialHeight + extraHeight, EXACTLY)
+      )
+      setPadding(paddingLeft, initialPadding + extraHeight, paddingRight, paddingBottom)
     }
-    layoutParams.height = initialHeight + extraHeight
-    setPadding(paddingLeft, initialPadding + extraHeight, paddingRight, paddingBottom)
+  }
+
+  override fun onAttachedToWindow() {
+    super.onAttachedToWindow()
+    requestApplyInsets()
   }
 }
